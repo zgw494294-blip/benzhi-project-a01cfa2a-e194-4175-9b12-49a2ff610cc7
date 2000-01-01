4a28e43cd84f3311761a7c113d25f95c69df75e7
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for a01cfa2a-e194-4175-9b12-49a2ff610cc7-bug-3

--- internal/store/repository.go
+++ internal/store/repository.go
@@ -74,25 +74,19 @@ func (r *SQLiteRepository) Save(ctx context.Context, c *domain.InspectionCase, p
 	if changed != 1 {
 		return domain.ErrConflict
 	}
-	if err := tx.Commit(); err != nil {
-		return err
-	}
-
-	projectionTx, err := r.db.BeginTx(ctx, nil)
-	if err != nil {
-		return err
-	}
-	defer projectionTx.Rollback()
-	if err := writeProjections(ctx, projectionTx, c); err != nil {
-		return err
-	}
-	if err := insertEventAndResult(ctx, projectionTx, event, idempotencyKey, result); err != nil {
+	if err := writeProjections(ctx, tx, c); err != nil {
 		if isConstraint(err) {
 			return domain.ErrConflict
 		}
 		return err
 	}
-	return projectionTx.Commit()
+	if err := insertEventAndResult(ctx, tx, event, idempotencyKey, result); err != nil {
+		if isConstraint(err) {
+			return domain.ErrConflict
+		}
+		return err
+	}
+	return tx.Commit()
 }
 
 func (r *SQLiteRepository) List(ctx context.Context) ([]domain.InspectionCase, error) {
